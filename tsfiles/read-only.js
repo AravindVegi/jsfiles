eefda65deb90dc4eb666a62fe966d636fb01874b
AravindVegi <101159327+AravindVegi@users.noreply.github.com>
2022-03-22 14:07:39 +0530

    Add files via upload

---
+++ tsfiles/read-only.js
@@ -0,0 +1,14 @@
+var personReadDemo = /** @class */ (function () {
+    function personReadDemo() {
+        this.name = 'vegi';
+        this.middleName = ' vegi';
+        constructor()
+    }
+    return personReadDemo;
+}());
+var obj = new personReadDemo();
+obj.name = ' man';
+console.log(obj.name);
+console.log(obj.name = ' aravind')
+obj.middleName = 'middleName';
+console.log(obj.middleName);
