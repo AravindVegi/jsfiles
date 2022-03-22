897556a83b1b2d18e3cf3c0b423e7f1bc7225c8f
AravindVegi <101159327+AravindVegi@users.noreply.github.com>
2022-03-22 15:24:48 +0530

    Add files via upload

---
+++ jspathfiles/var-declaration.js
@@ -0,0 +1,21 @@
+var a;
+var b;
+var c;
+var d;
+console.log("a,b,c,d");
+
+var a=2,c=3,d=4,b=5;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+var fname="vegi",lname="Aravind",country="India";
+
+
+console.log(fname);
+console.log(lname);
+console.log(country);
+
+//  prints in the same line
+console.log(fname,lname,mstatus,country,age);
